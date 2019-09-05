69dd563e2c1fe2cd0cdb67af26358356c0af3fb2
Luiz Mendes <developer.luizmendes@gmail.com>
2019-09-05 20:35:26 -0300

    busca finalizada

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,33 @@
 import fetch from 'isomorphic-unfetch'
+import React,{useState} from 'react'
 
-const Index = ({ reddit }) => (
+const Index = ({ reddit }) => {
+  
+  let __redditDefault = reddit
+  
+  const [rjson,setRjson] = useState(__redditDefault)
 
-  <ul>
-    
-    {reddit.data.children.map((p, i) => (
-      <li key={i}>{p.data.title}</li>
-    ))}
-  </ul>
-)
+  async function teste(tag){
+    const response = await fetch(`http://localhost:3000/api/reddit/${tag}`)
+    const reddit = await response.json()
+    setRjson(reddit)
+  }
+
+  return <div>
+
+          <input id="tag" type="text"/>
+
+          <input type="button" value="buscar" onClick={()=>teste(document.querySelector('#tag').value)}/>
+
+          <ul>
+            {rjson.data.children.map((p, i) => (
+            <li key={i}>{p.data.title}</li>
+            ))}
+          </ul>
+        </div> 
+  }
+
+
 
 Index.getInitialProps = async () => {
   const response = await fetch(`http://localhost:3000/api/reddit/zelda`)
